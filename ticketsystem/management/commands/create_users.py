a9f0d3e6f23d832e5095c061fe6905985386ccf2
Sina Şehlaver <28963369+sinasehlaver@users.noreply.github.com>
2024-05-11 17:55:22 +0300

    Update create_users.py

--- ticketsystem/management/commands/create_users.py
+++ ticketsystem/management/commands/create_users.py
@@ -1,12 +1,31 @@
-import django.contrib.auth
-User = django.contrib.auth.get_user_model()
-user = User.objects.create_user(username='mimarlik', password='companyticket22')
-user.save()
-user = User.objects.create_user(username='kutuphane', password='companyticket22')
-user.save()
-user = User.objects.create_user(username='carsi', password='companyticket22')
-user.save()
-user = User.objects.create_user(username='hazirlik', password='companyticket22')
-user.save()
-user = User.objects.create_user(username='disari', password='companyticket22')
-user.save()
+from django.core.management import CommandError
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = "Create a user, and allow password to be provided"
+    requires_migrations_checks = True
+  
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--password",
+            dest="password",
+            default="companyticket22",
+            help="Specifies the password for the users.",
+        )
+ 
+    def handle(self, *args, **options):
+        password = options.get("password")
+        
+        user = User.objects.create_user(username='mimarlik', password=password)
+        user.save()
+        user = User.objects.create_user(username='kutuphane', password=password)
+        user.save()
+        user = User.objects.create_user(username='carsi', password=password)
+        user.save()
+        user = User.objects.create_user(username='hazirlik', password=password)
+        user.save()
+        user = User.objects.create_user(username='disari', password=password)
+        user.save()
+        return "Successfully created users"
